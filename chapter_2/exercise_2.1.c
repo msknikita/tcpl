cb89bb7c14f64765910e827deae9419577304198
Nikita <aforgarbage@yandex.ru>
2018-12-18 19:54:38 +0300

    add description for 2.1

--- chapter_2/exercise_2.1.c
+++ chapter_2/exercise_2.1.c
@@ -2,6 +2,10 @@
 #include<limits.h>
 #include<float.h>
 
+/* Write a program to determine the ranges of char, short, int, and long variables, both signed and unsigned,
+ * by printing appropriate values from standard headers and by direct computation 
+ */ 
+
 int main(void)
 {
 	/* 1 */
