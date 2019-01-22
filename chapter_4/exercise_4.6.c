77bdb82bb706fbbb5a64a0bcd71ee00af4650497
Nikita <aforgarbage@yandex.ru>
2019-01-22 18:23:02 +0300

    add exercises 4.5, 4.6 and delete txt file

---
+++ chapter_4/exercise_4.6.c
@@ -0,0 +1,199 @@
+/* Add commands for handling variables(It's easy to provide twenty-six variables with single-letter names).
+ * Add a variable for the most recently printed value.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MAXOP 100
+#define NUMBER '0'
+#define PEEK 'p'
+
+int getop(char s[]);
+void push(double);
+double pop(void);
+void clear(void);
+
+int main()
+{
+	int type, i, var = 0;
+	double op1, op2, v = 0.0;
+	char s[MAXOP];
+	double variable[26];
+
+	for (i = 0; i < 26; ++i)
+		variable[i] = 0.0;
+
+	while ((type = getop(s)) != EOF) {
+		switch (type)
+		{
+		case NUMBER:
+			push(atof(s));
+			break;
+		case '+':
+			push(pop() + pop());
+			break;
+		case '*':
+			push(pop() * pop());
+			break;
+		case '-':
+			op2 = pop();
+			push(pop() - op2);
+			break;
+		case '/':
+			op2 = pop();
+			if (op2 != 0.0)
+				push(pop() / op2);
+			else
+				printf("error: zero divisor\n");
+			break;
+		case '%':
+			op2 = pop();
+			if (op2 != 0.0) {
+				op1 = pop();
+				push(op1 - op2 * floor(op1 / op2));
+			}
+			else
+				printf("Error: division by zero");
+			break;
+		case PEEK:
+			op2 = pop();
+			printf("\t%.8g\n", op2);
+			push(op2);
+			break;
+		case 'd':
+			op2 = pop();
+			push(op2);
+			push(op2);
+			break;
+		case 'c':
+			clear();
+			break;
+		case 's':
+			op1 = pop();
+			op2 = pop();
+			push(op1);
+			push(op2);
+			break;
+		case 'w':
+			op2 = pop();
+			if (op2 > 0) {
+				op1 = pop();
+				if (op1 != 0)
+					push(pow(op1, op2));
+				else
+					printf("domain error: x = 0\n");
+			}
+			else
+				printf("domain error: y <= 0\n");
+			break;
+		case '=':
+			pop();
+			if (var >= 'A' && var <= 'Z')
+				variable[var - 'A'] = pop();
+			else
+				printf("error: novariablename\n");
+			break;
+		case '\n':
+			v = pop();
+			printf("\t%.8g\n", v);
+			break;
+		default:
+			if (type >= 'A'&&type <= 'Z')
+				push(variable[type - 'A']);
+			else if (type == 'v')
+				push(v);
+			else
+				printf("error: unknown command %s\n", s);
+			break;
+		}
+		var = type;
+	}
+
+	return 0;
+}
+
+#define MAXVAL 100
+
+int sp = 0;
+double val[MAXVAL];
+
+void push(double f)
+{
+	if (sp < MAXVAL)
+		val[sp++] = f;
+	else
+		printf("error: stack full, can't push %g\n", f);
+}
+
+double pop(void)
+{
+	if (sp > 0)
+		return val[--sp];
+	else {
+		printf("error: stack empty\n");
+		return 0.0;
+	}
+}
+
+void clear(void)
+{
+	sp = 0;
+}
+
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+int getop(char s[])
+{
+	int i = 0, c, t;
+	while ((s[i] = c = getch()) == ' ' || c == '\t');
+	if (!isdigit(c) && c != '.') {
+		if (c == '-') {
+			if (isdigit(t = getch()) || t == '.') {
+				c = t;
+				s[++i] = c;
+			}
+			else {
+				ungetch(t);
+				return c;
+			}
+		}
+		else
+			return c;
+	}
+
+	if (isdigit(c))
+		while (isdigit(s[++i] = c = getch()));
+
+	if (c == '.')
+		while (isdigit(s[++i] = c = getch()));
+
+	s[i] = '\0';
+
+	if (c != EOF)
+		ungetch(c);
+
+	return NUMBER;
+}
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void)
+{
+	return(bufp > 0) ? buf[--bufp] : getchar();
+} 
+
+void ungetch(int c)
+{
+	if (bufp >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+}
